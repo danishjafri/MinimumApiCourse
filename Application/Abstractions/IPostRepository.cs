d371fd2dbcf6142a30f29aa4ae5b6b18ffe6f9c5
Danish Jafri <danishjafri@outlook.com>
2023-04-10 09:41:59 +0100

    Added Promise for the Post repository layer.

---
+++ Application/Abstractions/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Abstractions
+{
+    public interface IPostRepository
+    {
+        Task<ICollection<Post>> GetAllPosts();
+        Task<Post> GetPostById(int postId);
+        Task<Post> CreatePost(Post post);
+        Task<Post> UpdatePost(string updatedContent, int postId);
+        Task DeletePost(int postId);
+    }
+}
