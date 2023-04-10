72fdab03a3a8112c1960037e63969601890d00db
Danish Jafri <danishjafri@outlook.com>
2023-04-10 10:51:59 +0100

    Added connection string, added EF Core pakages. Using dependency injection, setup a working sketon.

--- MinimumApi/Program.cs
+++ MinimumApi/Program.cs
@@ -1,5 +1,15 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<SocialDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
